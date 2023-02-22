5d627806ef8f9de44a18e1bfba09a036953e7335
ksenia-5 <xenia.germanovich@gmail.com>
2023-02-22 23:28:43 +0000

    refactored code

--- calculate_tax/calculate_tax.py
+++ calculate_tax/calculate_tax.py
@@ -7,7 +7,27 @@ The function can process a different number of bands, looping through bands
 to calculate tax payble at different rates for each band.
 '''
 
-def calculate_tax(price, bands, rates, first_time_relief = 175000, first_time_buyer = False,  additional_dwelling = False, ads_band = [40000, 0.06]):
+residential_band_rates = {
+    'bands' : [145000,250000,325000,750000],
+    'rates' : [0.02, 0.05, 0.1, 0.12],
+    'first_time_relief' : 175000,
+    }
+
+non_residential_band_rates = {
+    'bands' : [15000,250000],
+    'rates' : [0.0, 0.05],
+    'first_time_relief' : 175000,
+    'ads_band' : {'threshold' : 40000, 'rate' : 0.06}
+    }
+
+
+ads_band_rate = {
+    'threshold' : 40000, 'rate' : 0.06
+    }
+
+
+def calculate_lbtt(price, first_time_buyer = False, band_rates = residential_band_rates):
+
     '''
     Function calculates LBTT payable and the additional dwelling supplement (ADS), 
     if  the buyer will have two homes at the end of the transaction.
@@ -15,6 +35,12 @@ def calculate_tax(price, bands, rates, first_time_relief = 175000, first_time_bu
     If the buyer is a first time buyer, first time buyer relief raises the tax free threshold to 175000, 
     reducing tax by up to £600
     '''
+
+    # Unpack tax rate data
+    bands = band_rates['bands']
+    rates = band_rates['rates']
+    first_time_relief = band_rates['first_time_relief']
+
     if first_time_buyer:
         bands[0] = first_time_relief
     
@@ -30,12 +56,43 @@ def calculate_tax(price, bands, rates, first_time_relief = 175000, first_time_bu
     # if purchase price is in the top band, apply top band rate to the amount above band boundary
     if price > bands[-1]:
         tax += (price - bands[-1] ) * rates[-1]
-
-    # if buying a second home, additional dwelling supplement (ADS) applies 
-    ads= 0
-
-    if additional_dwelling:
-        if price >= ads_band[0]:
-            ads = price * ads_band[1]
         
-    return (tax, ads)
+    return tax
+
+
+
+
+def calculate_ads(price, additional_dwelling = False, ads_band = ads_band_rate):
+    
+    '''
+    Function calculates additional dwelling supplement (ADS)
+    takes user values purchase price :int and additional_dwelling :bool,
+    tax authority data tax-free threshold :int and rate :float,
+    returns ads_payble :float 
+    '''
+
+    ads_payable = 0
+
+    if additional_dwelling and price >= ads_band['threshold']:
+        ads_payable = price * ads_band['rate']
+    
+    return ads_payable
+
+
+
+def calculate_tax(price, first_time_buyer = False, additional_dwelling = False, band_rates = residential_band_rates, ads_rate = ads_band_rate):
+    '''
+    Function calculates total tax due on residential property purchase.
+    Takes user data - price :int, first_time_buyer and additional_dwelling :bool
+    and tax authority data ads_threshold :int and ads_rate :float,
+    prints summary and returns total tax due.
+    '''
+    
+    lbtt_payable = calculate_lbtt(price, first_time_buyer, band_rates)
+    ads_payable = calculate_ads(price, additional_dwelling, ads_rate)
+    
+    total_tax = lbtt_payable + ads_payable
+    
+    print(f"LBTT due: {lbtt_payable}, ADS due: {ads_payable}... Total tax payable = £{total_tax}")
+    
+    return total_tax
--- test_calculate_tax.py
+++ test_calculate_tax.py
@@ -1,28 +1,63 @@
 import pytest
-from calculate_tax.calculate_tax import calculate_tax
+from calculate_tax.calculate_tax import calculate_tax, calculate_ads, calculate_lbtt
 
 # Scotland rates
-bands = [145000,250000,325000,750000]
-rates = [0.02, 0.05, 0.1, 0.12]
+# bands = [145000,250000,325000,750000]
+# rates = [0.02, 0.05, 0.1, 0.12]
 
-def test_lbtt_band_1():
-    assert calculate_tax(120000, bands, rates) == (0, 0)
+def test_calculate_ads():
+    assert calculate_ads(15000, additional_dwelling = True) == 0
 
-def test_lbtt_band_2():
-    assert calculate_tax(209000, bands, rates) == (1280, 0)
+def test_calculate_ads_2():
+    assert calculate_ads(40000, additional_dwelling = True) == 2400
 
-def test_lbtt_band_3():
-    assert calculate_tax(300000, bands, rates) == (4600, 0)
-
-def test_lbtt_band_4():
-    assert calculate_tax(686000, bands, rates) == (41950, 0)
-
-def test_lbtt_band_5():
-    assert calculate_tax(3700000, bands, rates) == (402350, 0)
+def test_calculate_ads_3():
+    assert calculate_ads(703000, additional_dwelling = True) == 42180
 
 
-# England rates
-# bands = [250000, 925000,1500000]
-# rates = [0.05, 0.1, 0.12]
-# first_time_relief = 425000
 
+# No first time buyer relief, no ADS
+
+def test_calculate_tax_band_0():
+    assert calculate_tax(120000) == 0
+
+def test_calculate_tax_band_1():
+    assert calculate_tax(180000) == 700
+
+def test_calculate_tax_band_2():
+    assert calculate_tax(312000) == 5200
+
+def test_calculate_tax_band_3():
+    assert calculate_tax(703000) == 43650
+
+def test_calculate_tax_band_4():
+    assert calculate_tax(1500000) == 138350
+
+
+
+# ADS, no first time buyer relief
+
+def test_calculate_tax_band_0_ads():
+    assert calculate_tax(120000, additional_dwelling = True) == 7200
+
+def test_calculate_tax_band_1_ads():
+    assert calculate_tax(312000, additional_dwelling = True) == 23920
+
+def test_calculate_tax_band_2_ads():
+    assert calculate_tax(703000, additional_dwelling = True) == 85830
+
+def test_calculate_tax_band_3_ads():
+    assert calculate_tax(1500000, additional_dwelling = True) == 228350
+
+
+
+# First time buyer
+
+def test_calculate_tax_first_time_band_0():
+    assert calculate_tax(175000, first_time_buyer = True, additional_dwelling = False) == 0
+
+def test_calculate_tax_first_time_band_2():
+    assert calculate_tax(312000, first_time_buyer = True, additional_dwelling = False) == 4600
+
+def test_calculate_tax_first_time_band_3():
+    assert calculate_tax(800000, first_time_buyer = True, additional_dwelling = False) == 53750
